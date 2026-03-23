8a9896b249d393bf4f58dabec834d0453bb2e78a
shivateja764 <dharmajishivatejashivateja4@gmail.com>
2026-03-23 18:20:14 +0530

    done

--- CityShop/frontend/src/frontEndConfig.js
+++ CityShop/frontend/src/frontEndConfig.js
@@ -1,7 +1,5 @@
 const config = {
-	SERVER_DOMAIN: "http://localhost:8000",
-	STRIPE_PUBLIC_KEY:
-		"pk_test_51TE1HEDXXey58gnH6cl05a1qGRJ8U2gsEwyVM9AnabyoAtWbNfNmigQJ0xeXmU6rlTd7nEx0SjW3vMryuoXTPeOP002dTW8dW7",
+	SERVER_DOMAIN: "http://localhost:8000",	
 };
 
 export default config;
