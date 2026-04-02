decbc047dc8d3bb1785c72b09977395e3df715dd
shivateja764 <dharmajishivatejashivateja4@gmail.com>
2026-04-02 17:01:13 +0530

    chabged

--- CityShop/backend/server.js
+++ CityShop/backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
-app.use("/uploads", express.static("uploads"));
+
 
 //port
 const PORT = process.env.PORT || 8000;
