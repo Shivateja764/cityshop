d49f963b23c4e9fb8e5f03ebde4cdc0ca8095816
shivateja764 <dharmajishivatejashivateja4@gmail.com>
2026-04-05 12:18:00 +0530

    cors fixed

--- CityShop/backend/server.js
+++ CityShop/backend/server.js
@@ -13,7 +13,11 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+const cors = require("cors");
+app.use(cors({
+  origin: "https://cityshop-5rrx.vercel.app/",
+  credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 
 
