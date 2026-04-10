11681174c5474156861ea8f6b762d0824d754483
shivateja764 <dharmajishivatejashivateja4@gmail.com>
2026-04-10 15:13:03 +0530

    payment fixed

--- CityShop/frontend/src/frontEndConfig.js
+++ CityShop/frontend/src/frontEndConfig.js
@@ -1,5 +1,8 @@
 const config = {
-	SERVER_DOMAIN: "http://localhost:8000",	
+	// SERVER_DOMAIN: "http://localhost:8000",
+	
+VITE_APP_SERVER_DOMAIN:"https://cityshop-3.onrender.com/" 
+	
 };
 
 export default config;
