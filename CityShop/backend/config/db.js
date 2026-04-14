88121c3c18b26a5fd91416e281446fed49ec8b76
shivateja764 <dharmajishivatejashivateja4@gmail.com>
2026-04-14 14:39:44 +0530

    fixed payment

--- CityShop/backend/config/db.js
+++ CityShop/backend/config/db.js
@@ -3,16 +3,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
 	try {
-		mongoose.set("strictQuery", false);
+		mongoose.set("strictQuery", true);
 
 		await mongoose.connect(process.env.MONGO_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
+			
 		});
 		console.log("Database is connected");
 	} catch (error) {
 		console.log(`Error: ${error.message}`);
-		process.exit(1);
+		
 	}
 };
 
