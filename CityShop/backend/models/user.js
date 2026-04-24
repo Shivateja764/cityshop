209719428adf43e04e056a7a6765fcab5aa533b0
shivateja764 <dharmajishivatejashivateja4@gmail.com>
2026-04-24 19:06:07 +0530

    fixed bcrypt

--- CityShop/backend/models/user.js
+++ CityShop/backend/models/user.js
@@ -1,6 +1,7 @@
-const { Schema, model } = require("mongoose");
 
-const UserSchema = new Schema({
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
 	firstName: { type: String, required: true },
 	lastName: { type: String, required: true },
 	email: { type: String, unique: true, required: true },
@@ -8,4 +9,4 @@ const UserSchema = new Schema({
 	image: { type: String },
 });
 
-module.exports = model("User", UserSchema);
+module.exports = mongoose.models.User || mongoose.model("User", UserSchema);
